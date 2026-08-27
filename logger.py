5cfa6d27876700dfb7c86114cce9514141c183f2
sathiyamoorthy-max <sathiyamoorthy8020@gmail.com>
2026-08-27 23:27:28 +0530

    Create logger.py

---
+++ logger.py
@@ -0,0 +1,22 @@
+import logging
+import os
+from logging.handlers import RotatingFileHandler
+
+try:
+    os.remove("logs.txt")
+except:
+    pass
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(funcName)s() - Line %(lineno)d: %(name)s - %(message)s",
+    datefmt="%d-%b-%y %I:%M:%S %p",
+    handlers=[
+        RotatingFileHandler("logs.txt", mode="w+", maxBytes=5000000, backupCount=10),
+        logging.StreamHandler(),
+    ],
+)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
